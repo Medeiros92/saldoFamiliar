2b45a7964db9ff9e09e504a3b85e0fb27f4b8421
Medeiros92 <thalesbehringer@gmail.com>
2021-09-11 09:35:42 -0300

    Create Function soma

--- saldoFamiliar.js
+++ saldoFamiliar.js
@@ -4,7 +4,15 @@ let myArray = {
     despesas: [],
 }
 
+// Função soma de elementos do array
+function soma(array){
+    let tot = 0;
 
+    for(let value of array){
+        tot += value;
+    }
+    return tot;
+}
 
 
 
